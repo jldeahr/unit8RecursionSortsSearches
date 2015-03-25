66b60fe1a1f9ba02da5368fbbdfc335172b55a2c
jldeahr <jldeahr50@gmail.com>
2015-03-25 12:19:45 -0500

    It Works!!!
    I have some very random color generation occurring.

---
+++ FractalTreeLab/TreePanel.java
@@ -0,0 +1,110 @@
+//********************************************************************
+//  KochPanel.java       Author: Lewis/Loftus/Cocking
+//
+//  Represents a drawing surface on which to paint a Koch Snowflake.
+//********************************************************************
+
+import java.awt.*;
+import javax.swing.JPanel;
+import java.util.Random;
+
+public class TreePanel extends JPanel
+{
+    private final int PANEL_WIDTH = 950;
+    private final int PANEL_HEIGHT = 950;
+
+    private final int BX = 475, BY = 475;
+    private final int ANGLE1 = 0;
+    private final int ANGLE2 = 45;
+    private final int ANGLE3 = 90;
+    private final int ANGLE4 = 135;
+    private final int ANGLE5 = 180;
+    private final int ANGLE6 = 225;
+    private final int ANGLE7 = 270;
+    private final int ANGLE8 = 315;
+
+    private int current; //current order
+
+    //-----------------------------------------------------------------
+    //  Sets the initial fractal order to the value specified.
+    //-----------------------------------------------------------------
+    public TreePanel (int currentOrder)
+    {
+        current = currentOrder;
+        setBackground (Color.black);
+        setPreferredSize (new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+    }
+
+    //-----------------------------------------------------------------
+    //  Draws the fractal recursively. Base case is an order of 1 for
+    //  which a simple straight line is drawn. Otherwise three
+    //  intermediate points are computed, and each line segment is
+    //  drawn as a fractal.
+    //-----------------------------------------------------------------
+    public void drawTree (int order, int x1, int y1, double angle, Graphics page)
+    {
+        if (order == 0)
+        {
+
+        }
+        else
+        {
+            int x2 = x1 + (int) (Math.cos(Math.toRadians(angle)) * order * 10);    
+            int y2 = y1 + (int) (Math.sin(Math.toRadians(angle)) * order * 10);
+            page.drawLine(x1,y1,x2,y2);
+            drawTree(order-1,x2,y2,angle+20,page);
+            drawTree(order-1,x2,y2,angle-20,page);
+        }
+    }
+
+    //-----------------------------------------------------------------
+    //  Performs the initial calls to the drawTree method.
+    //-----------------------------------------------------------------
+    public void paintComponent (Graphics page)
+    {
+        super.paintComponent (page);
+        
+        page.setColor (randomColor());
+        drawTree (current, BX, BY, ANGLE1, page);
+        page.setColor (randomColor());
+        drawTree (current, BX, BY, ANGLE2, page);
+        page.setColor (randomColor());
+        drawTree (current, BX, BY, ANGLE3, page);
+        page.setColor (randomColor());
+        drawTree (current, BX, BY, ANGLE4, page);
+        page.setColor (randomColor());
+        drawTree (current, BX, BY, ANGLE5, page);
+        page.setColor (randomColor());
+        drawTree (current, BX, BY, ANGLE6, page);
+        page.setColor (randomColor());
+        drawTree (current, BX, BY, ANGLE7, page);
+        page.setColor (randomColor());
+        drawTree (current, BX, BY, ANGLE8, page);
+    }
+
+    //-----------------------------------------------------------------
+    //  Sets the fractal order to the value specified.
+    //-----------------------------------------------------------------
+    public void setOrder (int order)
+    {
+        current = order;
+    }
+
+    //-----------------------------------------------------------------
+    //  Returns the current order.
+    //-----------------------------------------------------------------
+    public int getOrder ()
+    {
+        return current;
+    }
+    
+    public Color randomColor()
+    {
+        Random rand = new Random();
+        float r = rand.nextFloat();
+        float g = rand.nextFloat();
+        float b = rand.nextFloat();
+        Color randomColor = new Color(r,g,b);
+        return randomColor;
+    }
+}
--- FractalTreeLab/TreeViewer.java
+++ FractalTreeLab/TreeViewer.java
@@ -1,24 +1,24 @@
 //********************************************************************
-//  KochSnowflakeViewer.java       Author: Lewis/Loftus/Cocking
+//  TreeViewer.java       Author: Deahr
 //
 //  Demonstrates the use of recursion.
-//  @gcschmit (19 July 2014): converted from an applet to an application
+//  @jldeahr (25 March 2015): converted from an applet to an application
 //********************************************************************
 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class KochSnowflakeViewer implements ActionListener
+public class TreeViewer implements ActionListener
 {
-    private final int WIDTH = 400;
-    private final int HEIGHT = 500;
+    private final int WIDTH = 1000;
+    private final int HEIGHT = 1000;
 
     private final int MIN = 1, MAX = 9;
 
     private JButton increase, decrease;
     private JLabel titleLabel, orderLabel;
-    private KochPanel drawing;
+    private TreePanel drawing;
     private JPanel panel, tools;
     private JFrame frame;
 
@@ -27,17 +27,17 @@ public class KochSnowflakeViewer implements ActionListener
     //-----------------------------------------------------------------
     public static void main(String[] args)
     {
-        KochSnowflakeViewer viewer = new KochSnowflakeViewer();
+        TreeViewer viewer = new TreeViewer();
     }
 
-    public KochSnowflakeViewer()
+    public TreeViewer()
     {
         tools = new JPanel ();
         tools.setLayout (new BoxLayout(tools, BoxLayout.X_AXIS));
         tools.setBackground (Color.yellow);
         tools.setOpaque (true);
 
-        titleLabel = new JLabel ("The Koch Snowflake");
+        titleLabel = new JLabel ("The Tree Fractal");
         titleLabel.setForeground (Color.black);
 
         increase = new JButton (new ImageIcon ("increase.gif"));
@@ -59,14 +59,14 @@ public class KochSnowflakeViewer implements ActionListener
         tools.add (Box.createHorizontalStrut (20));
         tools.add (orderLabel);
 
-        drawing = new KochPanel (1);
+        drawing = new TreePanel (1);
 
         panel = new JPanel();
         panel.add (tools);
         panel.add (drawing);
 
         frame = new JFrame();
-        frame.setTitle("Knoch Snowflake");
+        frame.setTitle("Tree Fractal");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(WIDTH, HEIGHT);
         frame.add(panel);
@@ -82,7 +82,7 @@ public class KochSnowflakeViewer implements ActionListener
         int order = drawing.getOrder();
 
         if (event.getSource() == increase)
-            order++;
+           order++; 
         else
             order--;
 
